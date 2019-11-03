594341259b7985ba91381d662ff8e7afa0f4dbbe
alan.ceng <microsonle@gmail.com>
2019-11-03 22:48:56 +0800

    first commit

---
+++ precache-manifest.661db9dc74dec2be7d2d718d8544e405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d724001a4ab3ce64f11f0f3f628f1a97",
+    "url": "/weiwen/index.html"
+  },
+  {
+    "revision": "d229da6d54db69b02d0a",
+    "url": "/weiwen/static/css/2.ec129f17.chunk.css"
+  },
+  {
+    "revision": "7f3f36ed30d25e8afd27",
+    "url": "/weiwen/static/css/main.0f692900.chunk.css"
+  },
+  {
+    "revision": "d229da6d54db69b02d0a",
+    "url": "/weiwen/static/js/2.0ba738ab.chunk.js"
+  },
+  {
+    "revision": "7f3f36ed30d25e8afd27",
+    "url": "/weiwen/static/js/main.7e61399d.chunk.js"
+  },
+  {
+    "revision": "0b8fcd6f950076a76201",
+    "url": "/weiwen/static/js/runtime~main.0c67fa68.js"
+  }
+]);
